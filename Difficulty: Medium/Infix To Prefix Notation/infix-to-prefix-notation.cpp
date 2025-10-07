631fd00cf1f4a4d7510a5d42c9941d6f15c4e15b
Dhruv041107 <159522576+Dhruv041107@users.noreply.github.com>
2025-10-07 13:18:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Infix To Prefix Notation/infix-to-prefix-notation.cpp
@@ -0,0 +1,53 @@
+class Solution {
+  public:
+    string infixToPrefix(string &s) {
+        // code here
+        reverse(s.begin(),s.end());
+        for(int i=0;i<s.length();i++){
+            if(s[i]==')')   s[i]='(';
+            else if(s[i]=='(')   s[i]=')';
+        }
+        
+        
+        
+        stack<char> st;
+        string ans = "";
+        unordered_map<char, int> priority = {
+            {'+', 1}, {'-', 1},
+            {'*', 2}, {'/', 2},
+            {'^', 3}
+        };
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s[i];
+            if (isalnum(c)) {
+                ans += c;
+            } 
+            else if (c == '(') {
+                st.push(c);
+            } 
+            else if (c == ')') {
+                while (!st.empty() && st.top() != '(') {
+                    ans += st.top();
+                    st.pop();
+                }
+                st.pop();
+            } 
+            else {
+                while (!st.empty() && st.top() != '(' &&
+                      ((c == '^' && priority[c] <= priority[st.top()]) ||
+                       (c != '^' && priority[c] < priority[st.top()]))) {
+                    ans += st.top();
+                    st.pop();
+                }
+                st.push(c);
+            }
+        }
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
